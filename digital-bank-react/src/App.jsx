1bc37dd2a412eea63ad075096580e49178be1f70
Ayra Baet <ayra.austine@gmail.com>
2026-04-06 09:58:46 +0800

    feat: add Hero and Feature sections to App.jsx

--- digital-bank-react/src/App.jsx
+++ digital-bank-react/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
+import FeatureSection from "./components/FeatureSection/FeatureSection";
 
 function App() {
   return(
     <>
       <Header />
+      <HeroSection />
+      <FeatureSection />
     </>
   );
 }
