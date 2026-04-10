38768fb225dbb87e7093efc264ece45eec23c10e
Ayra Baet <ayra.austine@gmail.com>
2026-04-10 09:20:53 +0800

    feat: create pages folder and restructure landing page

---
+++ digital-bank-react/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import HeroSection from "../components/HeroSection/HeroSection";
+
+function LandingPage() {
+    return(
+        <>
+            <Header />
+            <HeroSection />
+        </>
+    );
+}
+
+export default LandingPage;
