0194c3c23ca8ec1a46d419ac51dedd2118dddbb6
Ayra Baet <ayra.austine@gmail.com>
2026-04-15 16:27:50 +0800

    feat: pass feature props to Section component in LandingPage
    - Import Section and Features component into LandingPage
    
    - Pass features data as props to Section component
    
    - Enables reusable Section component to display feature items dynamically

--- digital-bank-react/src/pages/LandingPage.jsx
+++ digital-bank-react/src/pages/LandingPage.jsx
@@ -1,11 +1,22 @@
 import Header from "../components/Header/Header";
 import HeroSection from "../components/HeroSection/HeroSection";
+import Section from "../components/Section/Section";
+import Features from "../components/Features/Features"
 
 function LandingPage() {
     return(
         <>
             <Header />
             <HeroSection />
+
+            <Section
+                backgroundColor="section--gray-100"
+                title="Why choose Digitalbank?"
+                description="We leverage Open Banking to turn your bank account into your financial hub. Control 
+          your finances like never before."
+            >
+                <Features />
+            </Section>
         </>
     );
 }
