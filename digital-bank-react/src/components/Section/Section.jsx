fe9d43fcd0108e9950d8bf705ae8f6d45eaeba72
Ayra Baet <ayra.austine@gmail.com>
2026-05-13 14:19:52 +0800

    fix: add arial-labelledby to Section and id to heading for accessibility

--- digital-bank-react/src/components/Section/Section.jsx
+++ digital-bank-react/src/components/Section/Section.jsx
@@ -2,10 +2,13 @@ import "./Section.css";
 
 function Section({ backgroundColor, title, description, children }) {
     return(
-        <section className={backgroundColor}>
+        <section 
+            className={backgroundColor}
+            aria-labelledby={`${title}-heading`}
+        >
             <div className="container section__container">
                 <div className="section__header">
-                    <h2>{title}</h2>
+                    <h2 id={`${title}-heading`}>{title}</h2>
                     {description && <p>{description}</p>}
                 </div>
 
